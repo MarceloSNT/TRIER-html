e8b1e10a16b14ac38e889e7699d203f65c73e652
marcelo_machado152 <marcelodossantosmachado05@gmail.com>
2025-07-17 16:30:31 -0300

    Atividade senai

--- TRIER-html/Dia_14_07_2025/ATIVIDADE/conversorTemperatura.js
+++ TRIER-html/Dia_14_07_2025/ATIVIDADE/conversorTemperatura.js
@@ -1,6 +1,32 @@
-function converterTemperatura(){
+/*function converterTemperaturaC(){
     const temperaturaC = Number (document.getElementById("temperaturaC").value);
 
-    const convertido = (temperaturaC * 9/5) + 32;
-    window.alert(parseFloat("Temperatura convertida para Fahrenheit: °F "+convertido));
+    const convertidoC = (temperaturaC * 9/5) + 32;
+    window.alert(parseFloat("Temperatura convertida para Fahrenheit: °F "+convertidoC));
+}
+function converterTemperaturaF(){
+    const temperaturaF = Number(document.getElementById("temperaturaF").value);
+
+    const convertidoF = (temperaturaF-32)/1.8;
+     window.alert(parseFloat("Temperatura convertida para Fahrenheit: °F "+convertidoF));
+}*/
+function selecionarUnidade() {
+  const select = document.getElementById("UnidadeTemperatura");
+  const input = document.getElementById("inputTemperatura");
+  const unidadeEscolhida = select.valeu;
+
+  if (unidadeEscolhida.equal("celsius")) {
+    const Celsius = (input * 9) / 5 + 32;
+
+    window.alert(
+      parseFloat("Temperatura convertida para Fahrenheit: °F " + Celsius)
+    );
+  }
+  if (unidadeEscolhida == "fahrenheit") {
+    const Fahrenheit = (input - 32) / 1.8;
+
+    window.alert(
+      parseFloat("Temperatura convertida para Fahrenheit: °F " + Fahrenheit)
+    );
+  }
 }
